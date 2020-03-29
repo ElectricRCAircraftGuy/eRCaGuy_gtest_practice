e71a5c0a3b627fd03c180e8fb8c190d7b3ef6f70
Gabriel Staples <ercaguy@gmail.com>
2020-03-29 15:29:11 -0700

    Update readme; add symbolic link to googletest-release-1.10.0; add gmock_test1.cpp

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # eRCaGuy_gtest_practice
 googletest (gtest) &amp; googlemock (gmock) practice
+
+# Author
+Gabriel Staples  
+www.ElectricRCAircraftGuy.com
+
+# Status
+Work-in-progress
+
+# To Build:
+1. Install Bazel: https://docs.bazel.build/versions/master/install-ubuntu.html
+1. Download the [latest Googletest release from here](https://github.com/google/googletest/releases) (I used v1.10.0). Extract it. Now you have a "googletest-release-1.10.0" directory. Symbolically link it into this project as follows:
+    
+        cd eRCaGuy_gtest_practice/googletest
+        ln -si /path/to/folder/googletest-release-1.10.0 .
+
+1. Ensure you are in the "eRCaGuy_gtest_practice" dir, and run the bazel build command:
+
+        # todo: fill me in
+
---
+++ gmock_test1.cpp
@@ -0,0 +1,43 @@
+// From: https://gist.github.com/lukant/376df8c49a3aba8179879fadcea77dea
+
+#include <gmock/gmock.h>
+
+using namespace ::testing;
+
+struct SomeMock
+{
+    MOCK_CONST_METHOD1(foo, void(int));
+};
+
+TEST(Examples, TheSameArgumentsGoingToFail)
+{
+    SomeMock mock;
+
+    EXPECT_CALL(mock, foo(4)).Times(1);
+    EXPECT_CALL(mock, foo(4)).Times(1);
+
+    mock.foo(4);
+    mock.foo(4);
+}
+
+TEST(Examples, theTestWillFailWhenOnlyLastExpectancyWasSatisfied)
+{
+    SomeMock mock;
+
+    EXPECT_CALL(mock, foo(4)).Times(1);
+    EXPECT_CALL(mock, foo(4)).Times(2);
+
+    mock.foo(4);
+    mock.foo(4);
+}
+
+TEST(Examples, theTestWillFailWhenOnlyLastExpectancyWasSatisfied_theSameCardinality)
+{
+    SomeMock mock;
+
+    EXPECT_CALL(mock, foo(4)).Times(2);
+    EXPECT_CALL(mock, foo(4)).Times(2);
+
+    mock.foo(4);
+    mock.foo(4);
+}
