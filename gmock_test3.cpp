fd03e16e217bbc24f4783cfe74b125940dc482bf
Gabriel Staples <ercaguy@gmail.com>
2020-03-31 00:37:22 -0700

    gmock_test3.cpp: add sample output to the bottom of the file

--- gmock_test3.cpp
+++ gmock_test3.cpp
@@ -54,3 +54,46 @@ TEST(Examples, AfterExample)
     mock.foo(4); // call#1
     mock.foo(4); // call#2
 }
+
+/*
+SAMPLE OUTPUT:
+
+$ time bazel test --test_output=all --test_arg=--gtest_color=yes //:gmock_test3
+INFO: Analyzed target //:gmock_test3 (0 packages loaded, 0 targets configured).
+INFO: Found 1 test target...
+Target //:gmock_test3 up-to-date:
+  bazel-bin/gmock_test3
+INFO: Elapsed time: 0.278s, Critical Path: 0.01s
+INFO: 0 processes.
+INFO: Build completed successfully, 1 total action
+PASSED: //:gmock_test3 (see /home/gabriel/.cache/bazel/_bazel_gabriel/0dc09bc4a514eaff2885980178dd3bac/execroot/ercaguy_gtest_practice/bazel-out/k8-fastbuild/testlogs/gmock_test3/test.log)
+INFO: From Testing //:gmock_test3
+==================== Test output for //:gmock_test3:
+Running main() from gmock_main.cc
+[==========] Running 4 tests from 1 test suite.
+[----------] Global test environment set-up.
+[----------] 4 tests from Examples
+[ RUN      ] Examples.InSequenceExample
+[       OK ] Examples.InSequenceExample (1 ms)
+[ RUN      ] Examples.InSequenceExampleSecondApproach
+[       OK ] Examples.InSequenceExampleSecondApproach (0 ms)
+[ RUN      ] Examples.RetiresOnSaturationExample
+[       OK ] Examples.RetiresOnSaturationExample (0 ms)
+[ RUN      ] Examples.AfterExample
+[       OK ] Examples.AfterExample (0 ms)
+[----------] 4 tests from Examples (1 ms total)
+
+[----------] Global test environment tear-down
+[==========] 4 tests from 1 test suite ran. (1 ms total)
+[  PASSED  ] 4 tests.
+================================================================================
+//:gmock_test3                                                  (cached) PASSED in 0.1s
+
+Executed 0 out of 1 test: 1 test passes.
+INFO: Build completed successfully, 1 total action
+
+real    0m0.359s
+user    0m0.032s
+sys 0m0.019s
+
+*/
